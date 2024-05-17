a10e8163c8343185e598ae0cc8754a919dbef735
Mohammed Al-Marshudi <76883519+marshudi@users.noreply.github.com>
2024-05-17 13:29:04 +0400

    Delete client directory

--- client/src/ExampleData.js
+++
@@ -1,6 +0,0 @@
-const Users=[
-    {uname:"ABC", email:"abc@gmail.com",pass:"123456"},
-    {uname:"ABC", email:"cde@gmail.com",pass:"234567"},
-    {uname:"ABC", email:"def@gmail.com",pass:"345678"}
-]
-export default Users;
--- client/src/Validations/LoginValidation.js
+++
@@ -1,8 +0,0 @@
-import * as yup from 'yup'; 
-
-//create schema
-export const LoginValid=yup.object().shape({
-    //required check if empty or not 
-    username: yup.string() .required("username cannot be empty"), 
-    password: yup.string() .required("Password cannot be empty") .min(6),
-});
